a2883c989621b02c69e823128f63d4842ebd86b3
nikitha2109 <136753913+nikitha2109@users.noreply.github.com>
2023-07-06 15:11:51 -0500

    Update GreetingController.java

--- src/main/java/com/example/sample/GreetingController.java
+++ src/main/java/com/example/sample/GreetingController.java
@@ -2,7 +2,9 @@ package com.example.sample;
 
 
 public class GreetingController {
+    public static void main(String[] args){
     public String greeting() {
         return "Lambda is Executed Successfully with Version 1.0.3 !!";
     }
+}
 }
